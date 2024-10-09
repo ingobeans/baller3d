dbe9ac3763ee36ca36ddad0e7adff5195c1aae57
ingobeans <127751491+ingobeans@users.noreply.github.com>
2024-10-09 16:58:14 +0200

    add faces to map

--- script.js
+++ script.js
@@ -10,11 +10,6 @@ resizeCanvas();
 
 window.addEventListener("resize", resizeCanvas);
 
-function drawRect(x, y, width, height, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
-}
-
 function drawLine(x1, y1, x2, y2, color, width = 1) {
   //x2 = x1 + 1;
   //y2 = y1 + 1;
@@ -25,6 +20,22 @@ function drawLine(x1, y1, x2, y2, color, width = 1) {
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
+}
+
+function drawTriangle(x1, y1, x2, y2, x3, y3, color) {
+  ctx.beginPath();
+  ctx.moveTo(x1, y1);
+  ctx.lineTo(x2, y2);
+  ctx.lineTo(x3, y3);
+  ctx.closePath();
+
+  ctx.fillStyle = color;
+  ctx.fill();
+}
+
+function drawRect(x1, y1, x2, y2, x3, y3, x4, y4, color) {
+  drawTriangle(x1, y1, x2, y2, x3, y3, color);
+  drawTriangle(x2, y2, x3, y3, x4, y4, color);
 }
 
 function drawImg(img, x, y, width, height) {
@@ -42,16 +53,46 @@ level = [
   [1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [2, 2, 2, 2, 2],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 2, 2, 1],
+  [1, 1, 2, 2, 1],
+  [1, 1, 2, 1, 1],
+  [1, 1, 2, 1, 1],
+  [1, 1, 2, 1, 1],
+  [1, 1, 2, 1, 1],
+  [1, 1, 2, 1, 1],
+  [1, 2, 2, 1, 1],
+  [1, 2, 2, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [1, 1, 0, 0, 0],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [0, 0, 0, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
+  [1, 1, 1, 1, 1],
   [0, 1, 1, 1, 0],
   [0, 1, 1, 1, 0],
   [0, 1, 1, 1, 0],
   [0, 1, 1, 1, 0],
-  [0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0],
-  [0, 1, 0, 1, 0],
-  [0, 0, 0, 0, 0],
+  [0, 1, 1, 1, 0],
+  [0, 1, 1, 1, 0],
+  [1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1],
   [1, 1, 1, 1, 1],
@@ -62,8 +103,8 @@ function update() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
   drawPerspective();
   cameraZ += 1;
-  drawLine(canvas.width / 2, 0, canvas.width / 2, canvas.height, "#ff00ff");
-  drawLine(0, canvas.height / 2, canvas.width, canvas.height / 2, "#ff00ff");
+  //drawLine(canvas.width / 2, 0, canvas.width / 2, canvas.height, "#ff00ff");
+  //drawLine(0, canvas.height / 2, canvas.width, canvas.height / 2, "#ff00ff");
   requestAnimationFrame(update);
 }
 
@@ -73,9 +114,9 @@ const clamp = (a, min = 0, max = 1) => Math.min(max, Math.max(min, a));
 let blockWidth = 100;
 let blockLength = 40;
 let blockHeight = 20;
-let cameraZ = 0;
+let cameraZ = -80;
 let cameraX = 0;
-let aa = 75;
+let distanceFactor = 800;
 
 function project(position) {
   let mapCenterX = (level[0].length * blockWidth) / 2;
@@ -86,7 +127,6 @@ function project(position) {
   let z = position[2];
   x += cameraX;
   let distance = z - cameraZ;
-  let distanceFactor = blockLength * level.length;
   let distanceScaling = distance / distanceFactor;
   y += screenCenterY * 2 - lerp(z - cameraZ, screenCenterY, distanceScaling);
   x = lerp(x, mapCenterX, distanceScaling);
@@ -95,7 +135,7 @@ function project(position) {
 }
 
 function drawPerspective() {
-  for (let z = 0; z < level.length; z++) {
+  for (let z = level.length - 1; z >= 0; z--) {
     for (let x = 0; x < level[level.length - z - 1].length; x++) {
       let pointX = x * blockWidth;
       let pointZ = z * blockLength;
@@ -122,21 +162,28 @@ function drawPerspective() {
       if (z == 0) {
         l = "#fff";
       }
-
-      drawLine(point_lu[0], point_lu[1], point_lb[0], point_lb[1], l);
-      drawLine(point_ru[0], point_ru[1], point_rb[0], point_rb[1], "#f00");
-      drawLine(point_lu[0], point_lu[1], point_ru[0], point_ru[1], "#0f0");
-      drawLine(point_lb[0], point_lb[1], point_rb[0], point_rb[1], "#00f");
-
-      drawLine(point_lut[0], point_lut[1], point_lbt[0], point_lbt[1], l);
-      drawLine(point_rut[0], point_rut[1], point_rbt[0], point_rbt[1], "#f00");
-      drawLine(point_lut[0], point_lut[1], point_rut[0], point_rut[1], "#0f0");
-      drawLine(point_lbt[0], point_lbt[1], point_rbt[0], point_rbt[1], "#00f");
-
-      drawLine(point_lu[0], point_lu[1], point_lut[0], point_lut[1], l);
-      drawLine(point_ru[0], point_ru[1], point_ru[0], point_rut[1], "#f00");
-      drawLine(point_lb[0], point_lb[1], point_lb[0], point_lbt[1], "#0f0");
-      drawLine(point_rb[0], point_rb[1], point_rb[0], point_rbt[1], "#00f");
+      drawRect(
+        point_lut[0],
+        point_lut[1],
+        point_lu[0],
+        point_lu[1],
+        point_rut[0],
+        point_rut[1],
+        point_ru[0],
+        point_ru[1],
+        "#0f0"
+      );
+      drawRect(
+        point_lu[0],
+        point_lu[1],
+        point_lb[0],
+        point_lb[1],
+        point_ru[0],
+        point_ru[1],
+        point_rb[0],
+        point_rb[1],
+        "#000"
+      );
     }
   }
 }
